01be5410f27e9592fca3fa81c410c13296b7d2db
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-08-02 15:10:56 +0300

    Dollar Objects

---
+++ src/USD.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace App;
+class USD
+{
+    private ?float $amount = null;
+    public function __construct(float $amount)
+    {
+        $this->amount = $amount;
+    }
+
+    public function times(int $multiplier): USD
+    {
+        return new USD($this->amount * $multiplier);
+    }
+
+    public function equals(USD $usd): bool
+    {
+        return $this->amount === $usd->amount;
+    }
+}
--- tests/MultiCurrencyTest.php
+++ tests/MultiCurrencyTest.php
@@ -1,9 +1,12 @@
 <?php
 declare(strict_types=1);
 
-use App\GBP;
 
-class MultiCurrencyTest extends \PHPUnit\Framework\TestCase
+use App\GBP;
+use App\USD;
+use PHPUnit\Framework\TestCase;
+
+class MultiCurrencyTest extends TestCase
 {
     public function testMultiplication(): void
     {
@@ -12,9 +15,16 @@ class MultiCurrencyTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals(new GBP(15), $five->times(3));
     }
 
-    public function testEquality()
+    public function testEquality(): void
     {
         $this->assertTrue((new GBP(5))->equals(new GBP(5)));
         $this->assertFalse((new GBP(5))->equals(new GBP(6)));
+    }
+
+    public function testUSDMultiplication(): void
+    {
+        $five = new USD(5);
+        $this->assertEquals(new USD(10), $five->times(2));
+        $this->assertEquals(new USD(15), $five->times(3));
     }
 }
